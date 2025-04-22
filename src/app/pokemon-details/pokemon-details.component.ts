2911bfe5e4669cac4e6d7b641e194c1f89c2bd80
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-04-23 01:42:10 +0200

    Back button functionality

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-details',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './pokemon-details.component.html',
   styleUrl: './pokemon-details.component.css'
 })
+
 export class PokemonDetailsComponent {
+  pokemon: any;
+  isFavourite: boolean = false;
+
+  constructor (private route: ActivatedRoute, private router: Router) {
+    this.route.params.subscribe(params => {
+      console.log(params);
+    })
+  }
+
+  goBack(): void {
+    this.router.navigate(['/']);
+  }
 
 }
